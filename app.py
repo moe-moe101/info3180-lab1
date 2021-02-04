47d4c4ea2b6727545fbc4ce445a8ab1146a088c1
Moesha AMos <moemusiclover7@gmail.com>
2021-02-03 21:21:23 -0500

    created by first Flask app

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 '''
 @app.route('/')
 def home():
-    return 'My home Page'
+    return 'My home page'
 
 
 
@@ -29,5 +29,5 @@ def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host="127.0.0.1", port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
 
